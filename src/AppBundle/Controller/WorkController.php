b865baaae42f964b9f99acf40d747381d60080cb
PetkoPopov <poetpoet2014@mail.ru>
2020-12-25 14:12:46 +0200

    добавяне на работа и сетване на името

--- src/AppBundle/Controller/WorkController.php
+++ src/AppBundle/Controller/WorkController.php
@@ -4,70 +4,158 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Work;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
-
-
 /**
- *@Route("/work")
+ * Work controller.
  *
+ * @Route("work")
  */
- class WorkController extends Controller
+class WorkController extends Controller
 {
     /**
-     * @Route("/new" , name="new_type" ,methods={"GET" , "POST"})
+     * Lists all work entities.
+     *
+     * @Route("/", name="work_index" , methods={"GET"})
+     *
      */
-    public function newAction()
+    public function indexAction()
     {
-        return $this->render('AppBundle:Work:new.html.twig', array(
-            // ...
+        $em = $this->getDoctrine()->getManager();
+
+        $works = $em->getRepository('AppBundle:Work')->findAll();
+
+        return $this->render('work/index.html.twig', array(
+            'works' => $works,
         ));
     }
 
     /**
+     * Creates a new work entity.
      *
-     * @Route("/showAll", name="show_all" , methods={"GET"})
+     * @Route("/new", name="work_new" , methods={"GET" , "POST"})
      *
      */
-    public function showAllAction()
+    public function newAction(Request $request)
     {
-   $em=
-            $this->getDoctrine()->getManager();
-        $allAcounters=$em->getRepository(
-            'AppBundle:Acounter'
-        )->findAll();
+        $queryString=$request->getQueryString();
+        $typeWork=explode('=',$queryString);
+        $work = new Work();
+        if( array_key_exists(1,$typeWork)){
+            $work->setTypeWork($typeWork[1]);
+        }
 
-        $allForPrint=[];
-foreach($allAcounters as $ac) {
-    if (!in_array($ac->getObjectName(), $allForPrint)) {
-        $allForPrint[] = $ac->getObjectName();
-    }
-}
+        $form = $this->createForm('AppBundle\Form\WorkType', $work);
+        $form->handleRequest($request);
 
-        return $this->render('Work/show_all.html.twig', array(
-            "all"=>$allForPrint
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($work);
+            $em->flush();
+
+            return $this->redirectToRoute('work_show', array('id' => $work->getId()));
+        }
+
+        return $this->render('work/new.html.twig', array(
+            'work' => $work,
+            'form' => $form->createView(),
         ));
     }
 
-     /**
-      * @Route("/createNew" , name="create_new" , methods={"GET"})
-      *
-      * @param Request
-      */
-    public function createNew(Request $request){
+    /**
+     * Finds and displays a work entity.
+     *
+     * @Route("/{id}", name="work_show" , methods={"GET"})
+     *
+     */
+    public function showAction(Work $work)
+    {
+        $deleteForm = $this->createDeleteForm($work);
 
-        echo "<pre>";
-        var_dump($request->all());
-        echo "</pre>";
-        die;
-$n=$request->getQueryString();
-$name=explode("=", $n);
-$work=new Work();
-$work->setTypeWork($name[1]);
-
-
+        return $this->render('work/show.html.twig', array(
+            'work' => $work,
+            'delete_form' => $deleteForm->createView(),
+        ));
     }
 
+    /**
+     * Displays a form to edit an existing work entity.
+     *
+     * @Route("/{id}/edit", name="work_edit" , methods={"GET", "POST"})
+     *
+     */
+    public function editAction(Request $request, Work $work)
+    {
+        $deleteForm = $this->createDeleteForm($work);
+        $editForm = $this->createForm('AppBundle\Form\WorkType', $work);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('work_edit', array('id' => $work->getId()));
+        }
+
+        return $this->render('work/edit.html.twig', array(
+            'work' => $work,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a work entity.
+     *
+     * @Route("/{id}", name="work_delete" , methods={"DELETE"})
+     *
+     */
+    public function deleteAction(Request $request, Work $work)
+    {
+        $form = $this->createDeleteForm($work);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($work);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('work_index');
+    }
+
+    /**
+     * Creates a form to delete a work entity.
+     *
+     * @param Work $work The work entity
+     *
+     * @return \Symfony\Component\Form\FormInterface The form
+     */
+    private function createDeleteForm(Work $work)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('work_delete', array('id' => $work->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+
+    /**
+     * @Route("/makeNew/{id}" , name="work_set_new" , methods={"GET"})
+     */
+    public function makeNewAction($id){
+
+        $em=$this->getDoctrine()->getManager();
+        $all=$em->getRepository(Work::class)
+            ->findAll();
+        $allWorks=[];
+        foreach ($all as $w){
+            if(!in_array($w->getTypeWork(),$allWorks)){
+                $allWorks[]=$w->getTypeWork();
+            }
+        }
+
+
+
+        return $this->render('work/setWork.html.twig',['all'=>$allWorks]);
+    }
 }
--- src/AppBundle/Form/WorkType.php
+++ src/AppBundle/Form/WorkType.php
@@ -2,10 +2,7 @@
 
 namespace AppBundle\Form;
 
-use Symfony\Component\DomCrawler\Field\ChoiceFormField;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\ChoiceList\View\ChoiceListView;
-use Symfony\Component\Form\ChoiceList\View\ChoiceView;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,13 +13,7 @@ class WorkType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
-//        echo "hello <pre>";
-//        var_dump($options);
-//        echo "</pre>";
-//        die;
-
-        $builder->add('typeWork',ChoiceFormField::class);
+        $builder->add('typeWork');
     }/**
      * {@inheritdoc}
      */
